ef482f9d5cfa5b83fc9cb00d8acd078b0715b4be
Thanh Hai Khong <khongthanhhai@gmail.com>
2024-06-05 12:35:36 +0700

    Player Controller

--- Sources/OPPlayer/OPPlayer.swift
+++ Sources/OPPlayer/OPPlayer.swift
@@ -3,3 +3,25 @@
 
 import Foundation
 import OPPlayerObjC
+
+public class PlayerController: NSObject, ObservableObject {
+    
+    // MARK: - Public Properties
+    
+    public let player: ZFPlayerController
+    public let containerView: UIView
+    public let controlView: ZFPlayerControlView
+    
+    // MARK: - Private Properties
+    
+    private let videoManager = ZFAVPlayerManager()
+    
+    // MARK: - Init
+    
+    public init(containerView: UIView, controlView: ZFPlayerControlView) {
+        self.containerView = containerView
+        self.controlView = controlView
+        self.player = ZFPlayerController(playerManager: videoManager, containerView: containerView)
+        self.player.controlView = controlView
+    }
+}
